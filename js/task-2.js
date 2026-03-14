1a72b88c1b2d231595c9f17b467a5862b126a723
sergijminda9 <sergijminda9@gmail.com>
2026-03-14 14:18:26 +0200

    fix: task-2-done

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,12 @@
 const calcAverageCalories = days => {
   if (days.length === 0) return 0;
+
   let total = 0;
+
   for (const day of days) {
     total += day.calories;
   }
+
   return total / days.length;
 };
 
